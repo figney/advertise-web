9dcd2421b107434dd1c980464a7bed0cbb25c518
make-money-master <money-001@mail.com>
2021-05-23 23:20:08 +0800

    修复代码格式

--- app/src/components/MoneyNumber.vue
+++ app/src/components/MoneyNumber.vue
@@ -1,25 +1,26 @@
 <template>
-  <span class="text-nowrap money-num">
-    <template v-if="day_dynamic">
-      <div :id="countToId"></div>
-    </template>
+    <span class="text-nowrap money-num">
+        <template v-if="day_dynamic">
+            <div :id="countToId"></div>
+        </template>
 
-    <template v-else>
-      <md-amount
-        :value="calcValue"
-        :has-separator="true"
-        :separator="separator"
-        :transition="showTransition"
-        :duration="parseFloat(showDuration)"
-        :precision="calcPrecision"
-        :is-round-up="false"
-      />
-      <span style="margin-left: 0.05rem" v-if="showK">{{ unit }}</span>
-      <span style="margin-right: 0.1rem; font-size: 100%" v-if="showCurrency">{{
-        system.default_currency
-      }}</span>
-    </template>
-  </span>
+        <template v-else>
+            <md-amount
+                :value="calcValue" :has-separator="true"
+                :separator="separator"
+                :transition="showTransition"
+                :duration="parseFloat(showDuration)"
+                :precision="calcPrecision"
+                :is-round-up="false"
+            />
+            <span style="margin-left: 0.05rem" v-if="showK">{{ unit }}</span>
+            <span
+                style="margin-right: 0.1rem; font-size: 100%"
+                v-if="showCurrency"
+                >{{ system.default_currency }}</span
+            >
+        </template>
+    </span>
 </template>
 
 <script>
@@ -30,157 +31,157 @@ import { CountUp } from "countup.js";
 import { Base } from "@/mixins";
 
 export default {
-  components: {
-    [Amount.name]: Amount,
-  },
-  props: {
-    value: {
-      type: [Number, String],
-      default: 0,
+    components: {
+        [Amount.name]: Amount,
     },
-    transition: {
-      type: Boolean,
-      default: false,
+    props: {
+        value: {
+            type: [Number, String],
+            default: 0,
+        },
+        transition: {
+            type: Boolean,
+            default: false,
+        },
+        duration: {
+            type: [Number, String],
+            default: 3000,
+        },
+        precision: {
+            type: [Number, String],
+            default: 0,
+        },
+        separator: {
+            type: String,
+            default: ",",
+        },
+        day_dynamic: {
+            type: Boolean,
+            default: false,
+        },
+        ck: {
+            type: Number,
+            default: 0,
+        },
     },
-    duration: {
-      type: [Number, String],
-      default: 3000,
+    data: () => {
+        return {
+            unit: "",
+            calcValue: 0,
+            showK: false,
+            showCurrency: true,
+            showTransition: false,
+            showDuration: 3000,
+            inited_secs: false,
+            countToId: random(12),
+        };
     },
-    precision: {
-      type: [Number, String],
-      default: 0,
+    mixins: [Base],
+    watch: {
+        left_secs() {
+            if (this.day_dynamic && !this.inited_secs) {
+                this.inited_secs = true;
+                this.calcDayDynamic();
+            }
+        },
+        value() {
+            this.$nextTick(() => {
+                this.updateShowValue();
+                if (this.day_dynamic) {
+                    this.calcDayDynamic();
+                }
+            });
+        },
+        transition() {
+            this.showTransition = this.transition;
+        },
+        duration() {
+            this.showDuration = this.duration;
+        },
+        day_dynamic() {
+            if (this.day_dynamic) {
+                this.calcDayDynamic();
+            }
+        },
     },
-    separator: {
-      type: String,
-      default: ",",
+    computed: {
+        ...mapState(["left_secs"]),
+        calcPrecision() {
+            let s = this.calcValue.toString();
+            if (s.indexOf(".") == -1) {
+                return 0;
+            } else {
+                let len = s.substring(s.indexOf(".") + 1).length;
+                return len < parseInt(this.precision) ? len : this.precision;
+            }
+        },
     },
-    day_dynamic: {
-      type: Boolean,
-      default: false,
+    mounted() {
+        this.$nextTick(() => {
+            this.updateShowValue();
+            if (this.day_dynamic) {
+                this.calcDayDynamic();
+            }
+        });
     },
-    ck: {
-      type: Number,
-      default: 0,
-    },
-  },
-  data: () => {
-    return {
-      unit: "",
-      calcValue: 0,
-      showK: false,
-      showCurrency: true,
-      showTransition: false,
-      showDuration: 3000,
-      inited_secs: false,
-      countToId: random(12),
-    };
-  },
-  mixins: [Base],
-  watch: {
-    left_secs() {
-      if (this.day_dynamic && !this.inited_secs) {
-        this.inited_secs = true;
-        this.calcDayDynamic();
-      }
-    },
-    value() {
-      this.$nextTick(() => {
-        this.updateShowValue();
-        if (this.day_dynamic) {
-          this.calcDayDynamic();
-        }
-      });
-    },
-    transition() {
-      this.showTransition = this.transition;
-    },
-    duration() {
-      this.showDuration = this.duration;
-    },
-    day_dynamic() {
-      if (this.day_dynamic) {
-        this.calcDayDynamic();
-      }
-    },
-  },
-  computed: {
-    ...mapState(["left_secs"]),
-    calcPrecision() {
-      let s = this.calcValue.toString();
-      if (s.indexOf(".") == -1) {
-        return 0;
-      } else {
-        let len = s.substring(s.indexOf(".") + 1).length;
-        return len < parseInt(this.precision) ? len : this.precision;
-      }
-    },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.updateShowValue();
-      if (this.day_dynamic) {
-        this.calcDayDynamic();
-      }
-    });
-  },
-  methods: {
-    updateShowValue() {
-      let value = parseFloat(this.value);
-      // if (this.system.show_suffix && this.value >= 1000000) {
-      //   this.calcValue = this.value / 1000000
-      //   this.showK = true
-      //   this.unit = 'M'
-      // }
-      if (this.system.show_suffix && value >= 1000) {
-        this.calcValue = value / 1000;
-        this.showK = true;
-        this.unit = "K";
-      } else {
-        this.calcValue = value;
-        this.unit = "";
-      }
-    },
-    calcDayDynamic() {
-      let total = this.value,
-        step = total / 86400,
-        passed = 86400 - this.left_secs,
-        startVal = passed * step,
-        decimalPlaces = this.calcDecimalPlaces(step);
+    methods: {
+        updateShowValue() {
+            let value = parseFloat(this.value);
+            // if (this.system.show_suffix && this.value >= 1000000) {
+            //   this.calcValue = this.value / 1000000
+            //   this.showK = true
+            //   this.unit = 'M'
+            // }
+            if (this.system.show_suffix && value >= 1000) {
+                this.calcValue = value / 1000;
+                this.showK = true;
+                this.unit = "K";
+            } else {
+                this.calcValue = value;
+                this.unit = "";
+            }
+        },
+        calcDayDynamic() {
+            let total = this.value,
+                step = total / 86400,
+                passed = 86400 - this.left_secs,
+                startVal = passed * step,
+                decimalPlaces = this.calcDecimalPlaces(step);
 
-      const countUp = new CountUp(this.countToId, this.value, {
-        startVal: startVal,
-        duration: this.left_secs,
-        useEasing: false,
-        decimalPlaces: decimalPlaces,
-      });
+            const countUp = new CountUp(this.countToId, this.value, {
+                startVal: startVal,
+                duration: this.left_secs,
+                useEasing: false,
+                decimalPlaces: decimalPlaces,
+            });
 
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        // console.log(countUp.error)
-      }
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                // console.log(countUp.error)
+            }
+        },
+        calcDecimalPlaces(n) {
+            let count = this.precision;
+            for (let i = 0; i < this.precision; i++) {
+                if (n >= 1 / 10 ** i) {
+                    count = i;
+                    break;
+                }
+            }
+            return count;
+        },
     },
-    calcDecimalPlaces(n) {
-      let count = this.precision;
-      for (let i = 0; i < this.precision; i++) {
-        if (n >= 1 / 10 ** i) {
-          count = i;
-          break;
-        }
-      }
-      return count;
-    },
-  },
 };
 </script>
 
 <style lang="scss">
 .money-num {
-  vertical-align: middle;
-  * {
     vertical-align: middle;
-    font-family: Montserrat, sans-serif !important;
-    // font-family: Orbitron, sans-serif !important;
-  }
+    * {
+        vertical-align: middle;
+        font-family: Montserrat, sans-serif !important;
+        // font-family: Orbitron, sans-serif !important;
+    }
 }
 </style>
--- app/src/components/NumChange.vue
+++ app/src/components/NumChange.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <span class="num-change" :id="elId" />
-  </div>
+    <div>
+        <span class="num-change" :id="elId" />
+    </div>
 </template>
 
 <script>
@@ -9,63 +9,63 @@ import stringRandom from "string-random";
 import { CountUp } from "countup.js";
 
 export default {
-  props: {
-    value: {
-      type: Number,
-      default: 0,
+    props: {
+        value: {
+            type: Number,
+            default: 0,
+        },
+        from: {
+            type: Number,
+            default: 0,
+        },
+        duration: {
+            type: Number,
+            default: 2,
+        },
     },
-    from: {
-      type: Number,
-      default: 0,
+    data: () => {
+        return {
+            elId: stringRandom(),
+        };
     },
-    duration: {
-      type: Number,
-      default: 2,
+    watch: {
+        value(newVal, oldVal) {
+            this.startCountUp(oldVal, newVal);
+        },
     },
-  },
-  data: () => {
-    return {
-      elId: stringRandom(),
-    };
-  },
-  watch: {
-    value(newVal, oldVal) {
-      this.startCountUp(oldVal, newVal);
+    mounted() {
+        this.$nextTick(() => {
+            this.startCountUp(this.from, this.value);
+        });
     },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.startCountUp(this.from, this.value);
-    });
-  },
-  methods: {
-    startCountUp(start = 0, end = 0) {
-      if (isNaN(start)) {
-        start = 0;
-      }
-      if (isNaN(end)) {
-        end = 0;
-      }
-      let options = {
-        startVal: start,
-        duration: this.duration,
-      };
-      let countUp = new CountUp(this.elId, end, options);
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error("countUp error:", countUp.error);
-      }
+    methods: {
+        startCountUp(start = 0, end = 0) {
+            if (isNaN(start)) {
+                start = 0;
+            }
+            if (isNaN(end)) {
+                end = 0;
+            }
+            let options = {
+                startVal: start,
+                duration: this.duration,
+            };
+            let countUp = new CountUp(this.elId, end, options);
+            if (!countUp.error) {
+                countUp.start();
+            } else {
+                console.error("countUp error:", countUp.error);
+            }
+        },
     },
-  },
 };
 </script>
 
 <style lang="scss">
 .num-change {
-  * {
-    font-family: Montserrat, sans-serif !important;
-    // font-family: Orbitron, sans-serif !important;
-  }
+    * {
+        font-family: Montserrat, sans-serif !important;
+        // font-family: Orbitron, sans-serif !important;
+    }
 }
 </style>
